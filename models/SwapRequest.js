c6ae4d15d22459b625ef4c2a8e93e244143daebe
Moustafa Bahaa <moustafabahaa2060@gmail.com>
2026-02-15 12:25:23 +0200

    model updated

--- models/SwapRequest.js
+++ models/SwapRequest.js
@@ -1,35 +1,49 @@
 const mongoose = require("mongoose");
 
-const SwapRequestSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const SwapRequestSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    // المهارة المطلوبة
+    skill: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Skill",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+
+    // --- حقول الشات ---
+    lastMessage: {
+      type: String,
+      default: "",
+    },
+    lastMessageSender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    unreadCount: {
+      type: Number,
+      default: 0,
+    },
   },
-
-  // المهارة المطلوبة فقط
-  skill: { type: mongoose.Schema.Types.ObjectId, ref: "Skill", required: true },
-
-  status: {
-    type: String,
-    enum: ["pending", "accepted", "rejected"],
-    default: "pending",
+  {
+    // دي بتهندل createdAt و updatedAt تلقائياً وبكفاءة أعلى
+    timestamps: true,
   },
-
-  // --- حقول الشات ---
-  lastMessage: { type: String, default: "" },
-  lastMessageSender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  unreadCount: { type: Number, default: 0 },
-  // -----------------------
-
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
-
-SwapRequestSchema.pre("save", function (next) {
-  this.updatedAt = Date.now();
-  next();
-});
+);
 
 module.exports = mongoose.model("SwapRequest", SwapRequestSchema);
